32aeb5caa2a17a5394f69a74004e3955be5da6df
Pascal Thomet <pthomet@ivsweb.com>
2017-05-13 15:45:10 +0200

    Add doctest support

---
+++ src/doctest_dynamic.cpp
@@ -0,0 +1,4 @@
+//This source file is required in order to link a dynamic library
+//It adds the doctest function definitions, but not the main() definition
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
